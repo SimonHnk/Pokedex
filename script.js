b1e75d73903fd2fa055d2ab352497eb9fe2c90e4
Simon Henke <henke.simon@yahoo.de>
2024-01-07 01:05:39 +0100

    search function & load more & style

--- script.js
+++ script.js
@@ -1,72 +1,122 @@
 let currentPokemon;
+let offset = 0;
+let renderArrayLength = 20;
+let renderStart = 1;
+let pokemonNames = [];
+let pokemonIds = [];
 
 
-async function loadPokemon() {
-    let url = 'https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0';
+async function loadPokemonArray() {
+    let url = `https://pokeapi.co/api/v2/pokemon/?limit=20&offset=${offset}`;
     let response = await fetch(url);
     let responseAsJson = await response.json();
     console.log(responseAsJson);
 
-    loadPokemonId(responseAsJson);
+    getPokemonUrl(responseAsJson);
 }
 
 
-async function loadPokemonId(responseAsJson) {
-    for (let i = 0; i < responseAsJson['results'].length; i++) {
+async function getPokemonUrl(responseAsJson) {
+    for (let i = 0, j = renderStart; i < responseAsJson['results'].length, renderArrayLength; i++, j++) {
         let pokemon = responseAsJson['results'][i];
-        let pokemonId = pokemon['url'];
-        console.log(pokemonId);
+        let pokemonUrl = await pokemon['url'];
 
-        await loadSinglePokemon(pokemonId, i);
+        loadSinglePokemon(pokemonUrl, j);
     }
 }
 
 
-async function loadSinglePokemon(pokemonId, i) {
-    let responsePokemon = await fetch(pokemonId);
+async function loadSinglePokemon(pokemonUrl, j) {
+    let responsePokemon = await fetch(pokemonUrl);
     currentPokemon = await responsePokemon.json();
     console.log(currentPokemon);
 
-    renderPokemon(i);
+    saveForSearch();
+    renderPokemon(j);
 }
 
 
-function renderPokemon(i) {
+function renderPokemon(j) {
     document.getElementById('pokemonIndex').innerHTML += `
-            <div id="pokemonCard${i}" onmouseover="tiltShake(${i})" onmouseout="tiltShakeOff(${i})" class="card pokemon-card ${currentPokemon['types'][0]['type']['name']}">
+            <div id="pokemonCard${j}" onmouseover="tiltShake(${j})" onmouseout="tiltShakeOff(${j})" class="card pokemon-card ${currentPokemon['types'][0]['type']['name']}">
                 <div class="card-body">
                     <h4 id="pokemonName" class="card-title title-color title-font mb-3">${currentPokemon['name'].charAt(0).toUpperCase() + currentPokemon['name'].slice(1)}</h4>
                     <div class="pokemon-min-info">
-                        <div id="pokemonType${i}" class="pokemon-min-info-type">
+                        <div id="pokemonType${j}" class="pokemon-min-info-type">
                             
                         </div>
-                        <img id="pokemonImg${i}" src="${currentPokemon['sprites']['other']['official-artwork']['front_default']}" class="img-fluid pokemon-img" alt="Pokemon">
+                        <img id="pokemonImg${j}" src="${currentPokemon['sprites']['other']['official-artwork']['front_default']}" class="img-fluid pokemon-img" alt="Pokemon">
                     </div>
                 </div>
                 <img src="./assets/img/pokeball.png" class="img-fluid bg-pokeball" alt="Pokeball">
             </div>
     `;
 
-    insertType(i);
+    insertType(j);
 }
 
 
-function insertType(i) {
+function insertType(j) {
     for (let t = 0; t < currentPokemon['types'].length; t++) {
         let type = currentPokemon['types'][t];
 
-        document.getElementById(`pokemonType${i}`).innerHTML += `
+        document.getElementById(`pokemonType${j}`).innerHTML += `
             <div class="pokemon-min-type">${type['type']['name'].charAt(0).toUpperCase() + type['type']['name'].slice(1)}</div>
         `;
     }
 }
 
 
-function tiltShake(i) {
-    document.getElementById(`pokemonImg${i}`).classList.add('tilt-shake');
+function saveForSearch() {
+    pokemonNames.push(currentPokemon['name']);
+    pokemonIds.push(currentPokemon['id']);
 }
 
 
-function tiltShakeOff(i) {
-    document.getElementById(`pokemonImg${i}`).classList.remove('tilt-shake');
+function tiltShake(j) {
+    document.getElementById(`pokemonImg${j}`).classList.add('tilt-shake');
+}
+
+
+function tiltShakeOff(j) {
+    document.getElementById(`pokemonImg${j}`).classList.remove('tilt-shake');
+}
+
+
+function loadMorePokemon() {
+    offset = offset + 20;
+    renderArrayLength = renderArrayLength + 20;
+    renderStart = renderStart + 20;
+
+    loadPokemonArray();
+}
+
+
+function searchPokemon() {
+    let searchQuerry = document.getElementById('searchInput').value
+    let match = searchQuerry.match(/\d+/);
+
+    if (match) {
+        for (let sId = 0; sId < pokemonIds.length; sId++) {
+            let id = +match[0];
+            let index = pokemonIds.indexOf(id);
+
+            if (index + 1 == sId + 1) {
+                document.getElementById(`pokemonCard${sId + 1}`).style.display = 'block';
+            } else {
+                document.getElementById(`pokemonCard${sId + 1}`).style.display = 'none';
+            }
+        }
+    } else {
+
+        for (let sName = 0; sName < pokemonNames.length; sName++) {
+            let pokeName = pokemonNames[sName];
+
+            if (pokeName.includes(searchQuerry.trim().toLowerCase())) {
+                document.getElementById(`pokemonCard${sName + 1}`).style.display = 'block';
+            } else {
+                document.getElementById(`pokemonCard${sName + 1}`).style.display = 'none';
+            }
+        }
+    }
 }

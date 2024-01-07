99b17c660c979f576dd9f99be7d87d5c9af60ff7
Simon Henke <henke.simon@yahoo.de>
2024-01-07 12:12:31 +0100

    fixed pkm load, background music

--- script.js
+++ script.js
@@ -1,66 +1,59 @@
 let currentPokemon;
-let offset = 0;
-let renderArrayLength = 20;
-let renderStart = 1;
+let loadPkmLimit = 20;
+let offset = 1;
 let pokemonNames = [];
 let pokemonIds = [];
+let pokemonArray = [];
+let backgroundSound = new Audio('./assets/sounds/rpg-town-loop.mp3');
 
 
-async function loadPokemonArray() {
-    let url = `https://pokeapi.co/api/v2/pokemon/?limit=20&offset=${offset}`;
-    let response = await fetch(url);
-    let responseAsJson = await response.json();
-    console.log(responseAsJson);
-
-    getPokemonUrl(responseAsJson);
+function init() {
+    loadPokemonArray();
 }
 
 
-async function getPokemonUrl(responseAsJson) {
-    for (let i = 0, j = renderStart; i < responseAsJson['results'].length, renderArrayLength; i++, j++) {
-        let pokemon = responseAsJson['results'][i];
-        let pokemonUrl = await pokemon['url'];
+async function loadPokemonArray() {
+    for (let i = offset; i <= loadPkmLimit; i++) {
+        let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        let response = await fetch(url);
+        currentPokemon = await response.json();
+        console.log(currentPokemon);
+        pokemonArray.push(currentPokemon);
 
-        loadSinglePokemon(pokemonUrl, j);
+        saveForSearch();  
+        renderPokemon(i);      
     }
 }
 
 
-async function loadSinglePokemon(pokemonUrl, j) {
-    let responsePokemon = await fetch(pokemonUrl);
-    currentPokemon = await responsePokemon.json();
-    console.log(currentPokemon);
-
-    saveForSearch();
-    renderPokemon(j);
-}
-
-
-function renderPokemon(j) {
+function renderPokemon(i) {
     document.getElementById('pokemonIndex').innerHTML += `
-            <div id="pokemonCard${j}" onmouseover="tiltShake(${j})" onmouseout="tiltShakeOff(${j})" class="card pokemon-card ${currentPokemon['types'][0]['type']['name']}">
+            <div id="pokemonCard${i}" onmouseover="tiltShake(${i})" onmouseout="tiltShakeOff(${i})" class="card pokemon-card ${currentPokemon['types'][0]['type']['name']}">
                 <div class="card-body">
+                <div class="card-headline-container">
                     <h4 id="pokemonName" class="card-title title-color title-font mb-3">${currentPokemon['name'].charAt(0).toUpperCase() + currentPokemon['name'].slice(1)}</h4>
+                    <h5>#${currentPokemon['id']}</h5>
+                    </div>
                     <div class="pokemon-min-info">
-                        <div id="pokemonType${j}" class="pokemon-min-info-type">
+                        <div id="pokemonType${i}" class="pokemon-min-info-type">
                             
                         </div>
-                        <img id="pokemonImg${j}" src="${currentPokemon['sprites']['other']['official-artwork']['front_default']}" class="img-fluid pokemon-img" alt="Pokemon">
+                        <img id="pokemonImg${i}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i}.png" class="img-fluid pokemon-img" alt="Pokemon">
                     </div>
                 </div>
                 <img src="./assets/img/pokeball.png" class="img-fluid bg-pokeball" alt="Pokeball">
             </div>
     `;
 
-    insertType(j);
+    insertType(i);
 }
 
 
-function insertType(j) {
+function insertType(i) {
     for (let t = 0; t < currentPokemon['types'].length; t++) {
         let type = currentPokemon['types'][t];
 
-        document.getElementById(`pokemonType${j}`).innerHTML += `
+        document.getElementById(`pokemonType${i}`).innerHTML += `
             <div class="pokemon-min-type">${type['type']['name'].charAt(0).toUpperCase() + type['type']['name'].slice(1)}</div>
         `;
     }
@@ -85,8 +78,7 @@ function tiltShakeOff(j) {
 
 function loadMorePokemon() {
     offset = offset + 20;
-    renderArrayLength = renderArrayLength + 20;
-    renderStart = renderStart + 20;
+    loadPkmLimit = loadPkmLimit + 20;
 
     loadPokemonArray();
 }
@@ -108,7 +100,6 @@ function searchPokemon() {
             }
         }
     } else {
-
         for (let sName = 0; sName < pokemonNames.length; sName++) {
             let pokeName = pokemonNames[sName];
 
@@ -119,4 +110,14 @@ function searchPokemon() {
             }
         }
     }
+}
+
+
+function playMusic() {
+    backgroundSound.play();
+}
+
+
+function offMusic() {
+    backgroundSound.pause();
 }
